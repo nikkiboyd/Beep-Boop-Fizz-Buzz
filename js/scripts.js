2fa7de7959a2f43c09a46b4a6e8ce75fb8dbb1af
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-08 15:54:52 -0700

    took quiz, cleaned up code

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,28 @@ function robotSpeak(inputNumber) {
   var parsedInputNumber = parseInt(inputNumber)
   var userString = inputNumber
   var userArray = userString.split("");
-
-    if (parsedInputNumber % 3 === 0) {
-      divisibleByThree(inputNumber);
-    } else if (userArray.includes("1")) {
-      containsOne(inputNumber);
-    } else if (userArray.includes("0")) {
-      containsZero(inputNumber);
-    } else {
-      countToInput(parsedInputNumber);
+  if (parsedInputNumber % 3 === 0) {
+    divisibleByThree(inputNumber);
+  } else if (userArray.includes("1")) {
+    containsOne(inputNumber);
+  } else if (userArray.includes("0")) {
+    containsZero(inputNumber);
+  } else {
+    countToInput(parsedInputNumber);
   };
 };
 
 function divisibleByThree(inputNumber) {
   var parsedInputNumber = parseInt(inputNumber)
   if (parsedInputNumber % 3 === 0)
-  // $("#divisible-by-three").show();
     document.getElementById("result").innerHTML = "I'm sorry, Dave. I'm afraid I can't do that.";
-  }
+}
 
 function containsOne(inputNumber) {
   var string = inputNumber
   var splitNumber = string.split("");
   var numberOne = splitNumber.includes("1")
   if (numberOne === true) {
-    // $("#boop").show();
     document.getElementById("result").innerHTML = "Boop!";
   }
 }
@@ -37,7 +34,6 @@ function containsZero(inputNumber) {
   var splitNumber = string.split("");
   var numberZero = splitNumber.includes("0")
   if (numberZero === true) {
-    // $("#beep").show();
     document.getElementById("result").innerHTML = "Beep!";
   }
 }
@@ -52,8 +48,8 @@ function countToInput(inputNumber) {
 }
 
 //User Interface Logic
-$(document).ready(function(){
-  $("form#beep-boop").submit(function(event){
+$(document).ready(function() {
+  $("form#beep-boop").submit(function(event) {
     event.preventDefault();
     var userInput = $("#user-input").val();
     var robotOutput = robotSpeak(userInput);
