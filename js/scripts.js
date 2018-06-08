b553dc3409c2f059008dac11151ebf78a509d78d
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-08 10:00:34 -0700

    add containsOne function to display Boop div if number contains a 1 in any index value

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 //Business Logic
 function robotSpeak() {
   }
-}
 
 function divisibleByThree(inputNumber) {
   if (inputNumber % 3 === 0) {
   $("#divisible-by-three").show();
+  }
+}
+
+function containsOne(inputNumber) {
+  var string = inputNumber
+  var numberOne = string.indexOf("1")
+  if (numberOne === 0) {
+    $("#boop").show();
   }
 }
 
