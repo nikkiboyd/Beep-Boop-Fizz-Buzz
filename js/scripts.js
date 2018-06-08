db28bbd5775e8e0cff2733bc0cb98c19c5bdef45
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-08 09:18:25 -0700

    add function to display div for results if input number is divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
+//Business Logic
+function robotSpeak() {
+  }
+}
+
+function divisibleByThree(inputNumber) {
+  if (inputNumber % 3 === 0) {
+  $("#divisible-by-three").show();
+  }
+}
+
 //User Interface Logic
 $(document).ready(function(){
   $("form#beep-boop").submit(function(event){
     event.preventDefault();
-    var userInput = $("#user-input").val();
+    var userInput = $(parseInt("#user-input").val());
     var robotOutput = robotSpeak(userInput);
     $("#result").text(robotOutput);
   });
+});
